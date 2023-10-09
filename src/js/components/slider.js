96fbf584a8a8425821037279b90a9338ae7aa37e
Nataliia Kudaieva <natkudaieva@gmail.com>
2023-10-09 20:11:28 +0300

    adding arrows to slider, add horizontal slider

--- src/js/components/slider.js
+++ src/js/components/slider.js
@@ -2,21 +2,29 @@ const slider = () => {
   const container = document.querySelector(".slider");
   const prev = document.querySelector(".slider__prev");
   const next = document.querySelector(".slider__next");
+  const down = document.querySelector(".slider__down");
   const item = document.querySelectorAll(".slider__item");
 
   let offset = 1;
 
-  const sizeOfItem = item[0].offsetWidth;
+  const widthOfItem = item[0].clientWidth;
+  const heightOfItem = item[0].clientHeight;
 
+  console.log(heightOfItem);
   const setWidth = (size, offset) => {
     let width = size * offset + "px";
     return (container.style.transform = `translateX(-${width})`);
   };
 
+  const setHeight = (size, offset) => {
+    let height = size * offset + "px";
+    return (container.style.transform = `translateY(-${height})`);
+  };
+
   next.addEventListener("click", () => {
     offset++;
     if (offset === item.length - 1) offset = 0;
-    setWidth(sizeOfItem, offset);
+    setWidth(widthOfItem, offset);
   });
 
   prev.addEventListener("click", () => {
@@ -24,7 +32,13 @@ const slider = () => {
 
     if (offset <= -1) offset = 0;
 
-    setWidth(sizeOfItem, offset);
+    setWidth(widthOfItem, offset);
+  });
+
+  down.addEventListener("click", () => {
+    offset++;
+    if (offset === item.length - 1) offset = 0;
+    setHeight(heightOfItem, offset);
   });
 };
 
