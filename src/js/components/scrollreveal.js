40382b122cf2077440df0764dd40e1ec5c66e1de
Nataliia Kudaieva <natkudaieva@gmail.com>
2023-10-23 20:20:36 +0300

    add scrolls v1

--- src/js/components/scrollreveal.js
+++ src/js/components/scrollreveal.js
@@ -1,10 +1,30 @@
 const scrolls = () => {
-  ScrollReveal().reveal(".intro", { delay: 250 });
-  ScrollReveal().reveal([".history", ".motto", ".arrangements"], {
+  const slideUp = {
+    distance: "150%",
+    origin: "bottom",
+    opacity: null,
     delay: 250,
-  });
-  ScrollReveal().reveal(".motto", { delay: 250 });
-  ScrollReveal().reveal(".arrangements", { delay: 250 });
+    easing: "ease-in",
+    interval: 600,
+  };
+
+  const slideRight = {
+    distance: "150%",
+    origin: "right",
+    opacity: 0.1,
+    delay: 500,
+    easing: "ease-out",
+    interval: 600,
+  };
+
+  ScrollReveal().reveal(".motto", slideUp);
+  // ScrollReveal().reveal([".history", ".motto", ".arrangements"], {
+  //   delay: 250,
+  // });
+
+  ScrollReveal().reveal(".arrangements__item", slideUp);
+
+  ScrollReveal().reveal(".activities__item", slideRight);
 };
 
 export default scrolls;
